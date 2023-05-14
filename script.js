1bb91e32503d5018269214ed6613f54017eff198
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-05-14 21:48:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const viewportWidth = window.innerWidth;
-const viewportHeight = window.innerHeight;
-console.log(viewportWidth, viewportHeight);
+const word = document.getElementById('word');
+console.log(word);
 
-const word = document.getElementById('wordAfter');
 const rect = word.getBoundingClientRect();
 console.log('top:', rect.top, 'bottom:', rect.bottom);
 
-rect.top >= 0 && rect.bottom <= window.innerHeight ? console.log('element is in Viewport') : console.log('elem is not in the viewport');
+//rect.top >= 0 && rect.bottom <= window.innerHeight ? word.classList.add('word') : word.classList.remove('word');
+
+if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+  word.classList.add('word1')
+  console.log(word)
+} else {
+  console.log('not in viewport')
+}
