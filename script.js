dc5f0fa8587cc32f90c9b916750d28447e32dd05
Issam Abou <113906543+Issam7Abou@users.noreply.github.com>
2023-05-15 22:07:40 +0100

    Add files via upload
    -fixed an issue where the nav items would scroll and cut the content of that particular section;
    -added more mobile responsiveness;

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Get the NavBar items and viewportWidth to later use on scroll function
+const navLinks = document.querySelectorAll('nav a');
+const viewportWidth = window.innerWidth;
+
+// Function that scroll to the right section without cutting the content of that section
+function scrollNav() {
+  //loop through the navLinks
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      // Prevent the links to open without the following code
+      e.preventDefault();
+      const sectionId = link.getAttribute('href');
+      const section = document.querySelector(sectionId);
+      const navbarHeight = document.querySelector('.nav-container').offsetHeight;
+      const sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+      // this will ensure that the scroll will not cut the content of that section
+      if (viewportWidth < 485) {
+        window.scrollTo({
+          top: sectionPosition - navbarHeight - 130,
+          behavior: 'smooth'
+        });
+      } else {
+        window.scrollTo({
+          top: sectionPosition - navbarHeight - 100,
+          behavior: 'smooth'
+        });
+      }
+    });
+  })
+}
+
+//On Load
+scrollNav();
+
+/*
 const word = document.getElementById('word');
 console.log(word);
 
@@ -12,3 +47,5 @@ if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
 } else {
   console.log('not in viewport')
 }
+*/
+
